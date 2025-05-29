a78b957f4e44095518aaa2331dd2b03dd00ef799
Ольга Костенко <you@example.com>
2025-05-29 21:40:21 +0500

    настроена админка для приложений

--- clients/admin.py
+++ clients/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
 
-# Register your models here.
+from clients.models import Clients, Message, Campaign
+
+
+@admin.register(Clients)
+class ClientsAdmin(admin.ModelAdmin):
+    list_display = ("id", "email", "full_name")
+    search_fields = ("email", "full_name")
+
+
+@admin.register(Message)
+class MessageAdmin(admin.ModelAdmin):
+    list_display = ("id", "topic",)
+    search_fields = ("topic",)
+
+
+@admin.register(Campaign)
+class CampaignAdmin(admin.ModelAdmin):
+    list_display = ("id", "status",)
+    search_fields = ("status",)
