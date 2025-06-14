c615adc9e99842666957fa5c4fc87918198a043d
Ольга Костенко <you@example.com>
2025-06-15 02:11:50 +0500

    Добавлены пути к новым контроллерам

--- clients/urls.py
+++ clients/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from clients.apps import ClientsConfig
-from clients.views import ClientListView, ClientDetailView, ClientCreateView, ClientUpdateView, ClientDeleteView
+from clients.views import (ClientListView, ClientDetailView, ClientCreateView, ClientUpdateView, ClientDeleteView,
+                           MessageListView, MessageDetailView, MessageCreateView, MessageUpdateView, MessageDeleteView)
 
 app_name = ClientsConfig.name
 
@@ -9,5 +10,11 @@ urlpatterns = [
     path("clients/<int:pk>/", ClientDetailView.as_view(), name="clients_detail"),
     path("clients/create/", ClientCreateView.as_view(), name="clients_create"),
     path("clients/<int:pk>/update/", ClientUpdateView.as_view(), name="clients_update"),
-    path("clients/<int:pk>/delete/", ClientDeleteView.as_view(), name="clients_delete")
+    path("clients/<int:pk>/delete/", ClientDeleteView.as_view(), name="clients_delete"),
+
+    path("message/", MessageListView.as_view(), name="message_list"),
+    path("message/<int:pk>/", MessageDetailView.as_view(), name="message_detail"),
+    path("message/create/", MessageCreateView.as_view(), name="message_create"),
+    path("message/<int:pk>/update/", MessageUpdateView.as_view(), name="message_update"),
+    path("message/<int:pk>/delete/", MessageDeleteView.as_view(), name="message_delete")
 ]
