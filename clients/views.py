4f9fcf0ae95471cec18f988967d81fcef7406ab5
Ольга Костенко <you@example.com>
2025-06-15 01:03:04 +0500

    Создан CRUD-операций для модели Clients

--- clients/views.py
+++ clients/views.py
@@ -1,5 +1,32 @@
 from django.shortcuts import render
+from clients.models import Clients
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+from clients.forms import ClientsForm
 
 
-def home(request):
-    return render(request, "base.html")
+class ClientListView(ListView):
+    model = Clients
+
+
+class ClientDetailView(DetailView):
+    model = Clients
+
+
+class ClientCreateView(CreateView):
+    model = Clients
+    form_class = ClientsForm
+    # fields = ("email", "full_name", "comment")
+    success_url = reverse_lazy('clients:clients_list')
+
+
+class ClientUpdateView(UpdateView):
+    model = Clients
+    form_class = ClientsForm
+    # fields = ("email", "full_name", "comment")
+    success_url = reverse_lazy('clients:clients_list')
+
+
+class ClientDeleteView(DeleteView):
+    model = Clients
+    success_url = reverse_lazy('clients:clients_list')
