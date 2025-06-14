8623ad9f80f500b7f7204e559f5294a64cbab656
Ольга Костенко <you@example.com>
2025-06-15 01:04:32 +0500

    Прописаны пути для модели Clients

--- clients/urls.py
+++ clients/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path
 from clients.apps import ClientsConfig
-from clients.views import home
+from clients.views import ClientListView, ClientDetailView, ClientCreateView, ClientUpdateView, ClientDeleteView
 
 app_name = ClientsConfig.name
 
-urlpatterns = [path("", home, name="home")]
+urlpatterns = [
+    path("", ClientListView.as_view(), name="clients_list"),
+    path("clients/<int:pk>/", ClientDetailView.as_view(), name="clients_detail"),
+    path("clients/create/", ClientCreateView.as_view(), name="clients_create"),
+    path("clients/<int:pk>/update/", ClientUpdateView.as_view(), name="clients_update"),
+    path("clients/<int:pk>/delete/", ClientDeleteView.as_view(), name="clients_delete")
+]
