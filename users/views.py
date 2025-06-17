456a3c528a5b2fbc804fe2c18a0cc88483dcbdcd
Ольга Костенко <you@example.com>
2025-06-17 15:20:26 +0500

    Добавлен контроллер для регистрации пользователя

--- users/views.py
+++ users/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
 
-# Create your views here.
+from users.models import User
+from users.forms import UserRegisterForm
+
+
+class UserCreateView(CreateView):
+    model = User
+    form_class = UserRegisterForm
+    success_url = reverse_lazy("users:login")
+
