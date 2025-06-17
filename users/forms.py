f90a59b54f75a52f0908f00167716d2b33af20ef
Ольга Костенко <you@example.com>
2025-06-17 13:44:45 +0500

    Добавлен класс для формы

---
+++ users/forms.py
@@ -0,0 +1,24 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.forms import BooleanField, ImageField
+from users.models import User
+
+
+class StyleFormMixin:
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for fild_name, fild in self.fields.items():
+            if isinstance(fild, BooleanField):
+                fild.widget.attrs["class"] = "form-check-input"
+            elif isinstance(fild, ImageField):
+                fild.widget.attrs["class"] = "form-control-file"
+            else:
+                fild.widget.attrs["class"] = "form-control"
+
+
+class UserRegisterForm(StyleFormMixin, UserCreationForm):
+    username = forms.CharField(max_length=50, required=True)
+
+    class Meta(UserCreationForm.Meta):
+        model = User
+        fields = ("email", "username", "password1", "password2")
