053d0afacb3e9df8b806f56ac24e2c7cac29e7cf
Ольга Костенко <you@example.com>
2025-06-18 14:59:30 +0500

    Создан контролер для регистрации пользователя с подтверждением

--- users/views.py
+++ users/views.py
@@ -1,12 +1,31 @@
+import secrets
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
 
+from config.settings import EMAIL_HOST_USER
 from users.models import User
 from users.forms import UserRegisterForm
+from django.core.mail import send_mail
 
 
 class UserCreateView(CreateView):
     model = User
     form_class = UserRegisterForm
     success_url = reverse_lazy("users:login")
+
+    def form_valid(self, form):
+        user = form.save()
+        user.is_active = False
+        token = secrets.token_hex(16)
+        user.token = token
+        user.save()
+        host = self.request.get_host()
+        url = f"http://{host}/users/email-confirm/{token}/"
+        send_mail(
+            subject="Подтверждение почты",
+            message=f"Здравствуйте, перейдите по ссылке для подтверждения почты: {url} ",
+            from_email=EMAIL_HOST_USER,
+            recipient_list=[user.email],
+        )
+        return super().form_valid(form)
 
