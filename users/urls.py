f0322218a4a4439c9102480ca57b3f9c7efd96c1
Ольга Костенко <you@example.com>
2025-06-27 15:00:19 +0500

    добавлены пути для восстановления пароля

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,12 @@
-from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.views import (
+    LoginView,
+    LogoutView,
+    PasswordResetDoneView,
+    PasswordResetCompleteView,
+)
 from django.urls import path
 from users.apps import UsersConfig
-from users.views import UserCreateView
+from users.views import UserCreateView, PasswordRecoveryView
 from users.services import email_verification
 
 app_name = UsersConfig.name
@@ -10,5 +15,18 @@ urlpatterns = [
     path("login/", LoginView.as_view(template_name="login.html"), name="login"),
     path("logout/", LogoutView.as_view(), name="logout"),
     path("register/", UserCreateView.as_view(), name="register"),
-    path("email-confirm/<str:token>/", email_verification, name="email-confirm")
+    path("email-confirm/<str:token>/", email_verification, name="email-confirm"),
+    path(
+        "password_recovery/", PasswordRecoveryView.as_view(), name="password_recovery"
+    ),
+    path(
+        "password-reset/done/",
+        PasswordResetDoneView.as_view(template_name="password_reset_done.html"),
+        name="password_reset_done",
+    ),
+    path(
+        "password-reset/complete/",
+        PasswordResetCompleteView.as_view(template_name="password_reset_complete.html"),
+        name="password_reset_complete",
+    ),
 ]
